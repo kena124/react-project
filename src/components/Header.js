6d51462fed6f5045ebe16cb083465e0b2f281ad5
kena1224 <kenabirtu19@gmail.com>
2023-12-15 02:16:39 -0800

    react js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/header.css"
+
+export const Header = () => {
+  return (
+    <div className='header'>
+        <box-icon name="rocket"></box-icon>
+    <h2> Game of billioners</h2>
+
+      </div>
+  )
+}
